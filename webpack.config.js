525d4f0eb0484a90dcff86bd4e6949e5d012dc3b
WhiteBai1 <WhiteBai1@outlook.com>
2025-10-29 01:27:24 +0800

    Publish Website

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,50 @@
 const CopyPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/app.ts",
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    target: ['web', 'es6'],
-    output: {
-        filename: "app.bundle.js",
-        path: path.join(__dirname, 'dist'),
-        clean: true
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader",
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{
-                from: path.join(__dirname, 'static'),
-                to: path.join(__dirname, 'dist')
-            }],
-        })
-    ],
-    stats: 'verbose'
+  mode : 'development',
+  entry : './src/app.ts',
+  resolve : {extensions : ['.ts', '.js']},
+  target : ['web', 'es6'],
+  output : {
+    filename : 'app.bundle.js',
+    // path : path.join(__dirname, 'dist'),
+    path : path.join(__dirname, 'docs'),
+    clean : true
+  },
+  devtool : 'source-map',
+  module : {
+    rules : [{
+      test : /\.ts$/,
+      loader : 'ts-loader',
+      exclude : /node_modules/,
+    }]
+  },
+  optimization : {
+    minimize : true,
+    minimizer : [new TerserPlugin()],
+  },
+  plugins : [new CopyPlugin({
+            patterns :
+                     [
+                       {
+                         from : path.join(__dirname, 'static/index.html'),
+                         to : path.join(__dirname, 'docs/index.html')
+                       },
+                       {
+                         from : path.join(__dirname, 'static/css'),
+                         to : path.join(__dirname, 'docs/css')
+                       },
+                       {
+                         from : path.join(__dirname, 'static/images'),
+                         to : path.join(__dirname, 'docs/images')
+                       },
+                       {
+                         from : path.join(__dirname, 'static/rulesets/default'),
+                         to : path.join(__dirname, 'docs/rulesets/default')
+                       },
+                     ],
+          })],
+  stats : 'verbose'
 };
